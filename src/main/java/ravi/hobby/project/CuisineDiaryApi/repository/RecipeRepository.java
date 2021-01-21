bcd7c06269cc2702546b5f637a6e87458f17340d
Raviteja Gubba <gubbaraviteja@gmail.com>
2021-01-21 22:32:27 +0100

    Removed unnecessary files and added reference to spring data docs

--- src/main/java/ravi/hobby/project/CuisineDiaryApi/repository/RecipeRepository.java
+++ src/main/java/ravi/hobby/project/CuisineDiaryApi/repository/RecipeRepository.java
@@ -3,5 +3,8 @@ package ravi.hobby.project.CuisineDiaryApi.repository;
 import org.springframework.data.repository.CrudRepository;
 import ravi.hobby.project.CuisineDiaryApi.model.Recipe;
 
+/**
+ * We are using Spring data module for data access layer. For more info, https://docs.spring.io/spring-data/data-commons/docs/2.4.3/reference/html/#reference
+ */
 public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
 }
